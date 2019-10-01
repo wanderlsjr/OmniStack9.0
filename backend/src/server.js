134bcfb31adf8be83879c05803e12fc0f04e4264
Wander Luiz <wanderlsj@gmail.com>
2019-10-01 14:58:51 -0300

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-r9hzp.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
